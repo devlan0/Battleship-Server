1100b7ec6674de21306a07f0ec66b9e992e23329
hennyroggy <felix243m@gmail.com>
2019-02-25 23:07:09 +0100

    didILose added

--- src/com/battleship/GameLogic.java
+++ src/com/battleship/GameLogic.java
@@ -1,5 +1,4 @@
-package com.battleship;
-
+mport com.battleship.UserData;
 
 public class GameLogic {
 
@@ -11,100 +10,167 @@ public class GameLogic {
     private static final int NUMBER_OF_SHIPS = 6;
 
     //Constructors
-    public GameLogic(UserData player1, UserData player2, int[][] defaultField1, int[][] defaultField2) {
+    public GameLogic(UserData player1, UserData player2, int[][] defaultField1, int[][] defaultField2)
+    {
         _player1 = player1;
         _player2 = player2;
         _field1 = defaultField1.clone();
         _field2 = defaultField2.clone();
     }
 
-    public GameLogic(UserData player1, UserData player2) {
+    public GameLogic(UserData player1, UserData player2)
+    {
         _player1 = player1;
         _player2 = player2;
-        for (int i[] : _field1) {
-            for (int j : i) {
-                j = 0;
+        for(int i[]: _field1)
+        {
+            for(int j: i)
+            {
+                j=0;
             }
         }
     }
 
     //public methods
-    public boolean shot(int x, int y, int playerIndex) {
-        switch (playerIndex) {
-            case 1:
-                if (_field1[x][y] == 1) //player1
-                {
-                    _field1[x][y] = -1;
-                    return true;
-                } else {
-                    return false;
-                }
-            case 2:
-                if (_field2[x][y] == 1) //player2
-                {
-                    _field2[x][y] = -1;
-                    return true;
-                } else {
-                    return false;
-                }
+    public boolean shot(int x, int y, int playerIndex)
+    {
+        switch (playerIndex)
+        {
+            case 1: if (_field1[x][y] == 1) //player1
+            {
+                _field1[x][y] = -1;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+            case 2: if (_field2[x][y] == 1) //player2
+            {
+                _field2[x][y] = -1;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
             default: //exception
         }
-        return false;
     }
 
 
-    public void setBattleships(int[] ships, int playerIndex) {
+    public void setBattleships(int[] ships, int playerIndex)
+    {
 
-        if (ships.length / 2 != NUMBER_OF_SHIPS || !(playerIndex == 1 || playerIndex == 2)) {
+        if(ships.length/2 != NUMBER_OF_SHIPS || !(playerIndex == 1 || playerIndex == 2))
+        {
             //execption
-        } else {
-            switch (playerIndex) {
-                case 1:
-                    for (int i = 0; i < ships.length - 2; i += 2) {
-                        if (ships[i] == ships[i + 2]) //the x coordinate of two following coordinates is the same => the ship is oriented vertically
+        }
+        else
+        {
+            switch(playerIndex)
+            {
+                case 1: for(int i = 0; i<ships.length-2; i+=2)
+                {
+                    if(ships[i] == ships[i+2]) //the x coordinate of two following coordinates is the same => the ship is oriented vertically
+                    {
+                        for(int l = 0; l < ships[i+1]-ships[i+3]+2; l++)
                         {
-                            for (int l = 0; l < ships[i + 1] - ships[i + 3] + 2; l++) {
-                                _field1[i][i + 1 + l] = 1;
+                            if(_field1[i][i+1+l] == 0)
+                            {
+                                _field1[i][i+1+l] = 1;
                             }
-                        } else if (ships[i + 1] == ships[i + 3]) // the y coordinate of two following coordinates is the same => the ship is faced horizontally
-                        {
-                            for (int l = 0; l < ships[i] - ships[i + 2] + 2; l++) {
-                                _field1[i][i + 1 + l] = 1;
+                            else
+                            {
+                                //exeption
                             }
-                        } else {
-                            //execption
                         }
                     }
-                case 2:
-                    for (int i = 0; i < ships.length - 2; i += 2) {
-                        if (ships[i] == ships[i + 2]) //the x coordinate of two following coordinates is the same => the ship is oriented vertically
+                    else if (ships[i+1] == ships[i+3]) // the y coordinate of two following coordinates is the same => the ship is faced horizontally
+                    {
+                        for(int l = 0; l < ships[i]-ships[i+2]+2; l++)
                         {
-                            for (int l = 0; l < ships[i + 1] - ships[i + 3] + 2; l++) {
-                                _field2[i][i + 1 + l] = 1;
+                            _if(_field2[i][i+1+l] == 0)
+                            {
+                                _field2[i][i+1+l] = 1;
                             }
-                        } else if (ships[i + 1] == ships[i + 3]) // the y coordinate of two following coordinates is the same => the ship is faced horizontally
-                        {
-                            for (int l = 0; l < ships[i] - ships[i + 2] + 2; l++) {
-                                _field2[i][i + 1 + l] = 1;
+                            else
+                            {
+                                //exeption
                             }
-                        } else {
-                            //execption
                         }
                     }
+                    else
+                    {
+                        //execption
+                    }
+                }
+                case 2: for(int i = 0; i<ships.length-2; i+=2)
+                {
+                    if(ships[i] == ships[i+2]) //the x coordinate of two following coordinates is the same => the ship is oriented vertically
+                    {
+                        for(int l = 0; l < ships[i+1]-ships[i+3]+2; l++)
+                        {
+                            _field2[i][i+1+l] = 1;
+                        }
+                    }
+                    else if (ships[i+1] == ships[i+3]) // the y coordinate of two following coordinates is the same => the ship is faced horizontally
+                    {
+                        for(int l = 0; l < ships[i]-ships[i+2]+2; l++)
+                        {
+                            _field2[i][i+1+l] = 1;
+                        }
+                    }
+                    else
+                    {
+                        //execption
+                    }
+                }
             }
+        }
+    }
+
+
+    public boolean didILose(int playerIndex)
+    {
+        boolean lost = true;
+        switch(playerIndex)
+        {
+            case 1: for(int i[]: _field1)
+            {
+                for(int j: i)
+                {
+                    if(j==1)
+                    {
+                        return false;
+                    }
+                }
+                return true;
+            }
+            case 2: for(int i[]: _field2)
+            {
+                for(int j: i)
+                {
+                    if(j==1)
+                    {
+                        return false;
+                    }
+                }
+                return true;
+            }
+            default: //exception
         }
     }
 
     //Hilfsmethoden:
 
-    public int[][] getField(int playerIndex) {
-        switch (playerIndex) {
-            case 1:
-                return _field1;
-            case 2:
-                return _field2;
-            default:
-                return null;
+    public int[][] getField(int playerIndex)
+    {
+        switch (playerIndex)
+        {
+            case 1: return _field1;
+            case 2: return _field2;
+            default: return null;
         }
     }
 
