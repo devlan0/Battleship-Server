d4da5085fc935e78c47397620c17ac3d38938475
hennyroggy <felix243m@gmail.com>
2019-02-25 21:37:50 +0100

    Added a shot method.

--- src/GameLogic.java
+++ src/GameLogic.java
@@ -1,13 +1,14 @@
 import com.battleship.UserData;
 
 public class GameLogic {
+
     private int[][] _field1;
     private int[][] _field2;
     //field integer meaning: 0 = water; 1 = living ship location; -1 = dead ship location; 2 = island
     private UserData _player1;
     private UserData _player2;
 
-
+    //Constructors
     public GameLogic(UserData player1, UserData player2, int[][] defaultField1, int[][] defaultField2)
     {
         _player1 = player1;
@@ -15,7 +16,6 @@ public class GameLogic {
         _field1 = defaultField1.clone();
         _field2 = defaultField2.clone();
     }
-
 
     public GameLogic(UserData player1, UserData player2)
     {
@@ -29,6 +29,7 @@ public class GameLogic {
             }
         }
     }
+    //public methods
 
     public boolean shot(int x, int y, int playerIndex)
     {
@@ -57,12 +58,16 @@ public class GameLogic {
     }
 
 
-    public int[][] getSpielfeld(int playerIndex)
+    public void
+
+
+
+    public int[][] getField(int playerIndex)
     {
         switch (playerIndex)
         {
-            case 1: return field1;
-            case 2: return field2;
+            case 1: return _field1;
+            case 2: return _field2;
             default: return null;
         }
     }
